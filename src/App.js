e9df06ecce241b4e638ecc23a4c7f7f716bb4c2c
Rahul Dathu <rahuldathathreya230197@gmail.com>
2023-03-16 07:16:08 +0530

    Start of the json-server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import AddTask from "./Components/AddTask";
 import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -38,10 +40,20 @@ function App() {
 const toggleReminder = (id) => {
   setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task ))
 }
+
+//Add Task
+const addTask= (task) => {
+  // console.log(task)
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
   return (
     
     <div className="container">
-      <Header title="TASK TRACKER"/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask} title="TASK TRACKER"/>
+      {showAddTask && <AddTask onAdd={addTask} /> }
       {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to show'}
       {/* <ReactOnly title='Only react branch should have this'></ReactOnly> */}
     </div>
